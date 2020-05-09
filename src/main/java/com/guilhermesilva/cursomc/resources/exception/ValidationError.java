db09535dde0c72f1b00884fbfc72b5a1084c61aa
Guilherme da Silva <guilherme.dasilvanascimento@gmail.com>
2020-05-09 00:48:33 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/guilhermesilva/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,25 @@
+package com.guilhermesilva.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FieldMessage> getErors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
